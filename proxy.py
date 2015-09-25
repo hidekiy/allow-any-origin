bd62f9cbaf375478fb7c14f2a570cafd0809fa84
Hideki Yamamura <hideki.yamamura@gmail.com>
2015-09-25 12:09:12 +0900

    add memcache

--- proxy.py
+++ proxy.py
@@ -5,43 +5,40 @@ import sys
 import hashlib
 from time import time
 
-import webapp2
+from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 from google.appengine.runtime import apiproxy_errors
+
+import webapp2
 
 class Quota(ndb.Model):
 	count = ndb.IntegerProperty()
 	reset_interval = ndb.IntegerProperty()
 	default_id = 'default'
 
+class UrlResp:
+	def __init__(self, res):
+		self.status_code = res.status_code
+		self.headers = res.headers
+		self.content = res.content
+	
 class HttpProxyHandler(webapp2.RequestHandler):
-	quota_dict = None
-	quota_reset_at = 0
-	quota_limits_refreshed_at = 0
-	quota_limits_refresh_interval = 60
-	quota_reset_interval = 0
-	quota_count_threshold = sys.maxint
+	quota = None
+	quota_refreshed_at = 0
+	quota_refresh_seconds = 60
+	urlfetch_cache_seconds = 60
 
 	@classmethod
-	def _prepare_quota(cls):
-		if time() - cls.quota_reset_at > cls.quota_reset_interval:
-			logging.info('reset quota_dict')
-			cls.quota_dict = dict()
-			cls.quota_reset_at = time()
-
-	@classmethod
-	def _update_quota_limits(cls):
-		if time() - cls.quota_limits_refreshed_at > cls.quota_limits_refresh_interval:
-			cls.quota_limits_refreshed_at = time()
+	def _refresh_quota(cls):
+		if time() - cls.quota_refreshed_at > cls.quota_refresh_seconds:
+			cls.quota_refreshed_at = time()
 			quota = Quota.get_by_id(Quota.default_id);
+			if not quota:
+				return
 
-			if quota:
-				quota_count = quota.count
-				quota_reset_interval = quota.reset_interval
-				logging.info('_update_quota_limits count=%d, reset_interval=%d', quota_count, quota_reset_interval)
-				cls.quota_count_threshold = quota_count
-				cls.quota_reset_interval = quota_reset_interval
+			logging.info('_refresh_quota %r', quota)
+			cls.quota = quota
 
 	def abort(self, *args, **kwargs):
 		super(HttpProxyHandler, self).abort(
@@ -50,40 +47,31 @@ class HttpProxyHandler(webapp2.RequestHandler):
 			**kwargs
 		)
 
-	def _check_quota(self, key):
-		key_hash = hashlib.sha1(key).digest()
-		quota_count = self.quota_dict.get(key_hash, 0) + 1
-		self.quota_dict[key_hash] = quota_count
-		logging.info('_check_quota: quota_count %d', quota_count)
+	def _check_quota(self, quota_key):
+		key = 'quota:%s:%d' % (
+			hashlib.sha1(quota_key).digest(),
+			int(time()) / self.quota.reset_interval
+		)
+		count = memcache.incr(key, initial_value=0)
+		logging.info('_check_quota: count %d', count)
 
-		if (quota_count > self.quota_count_threshold):
-			logging.warning('over internal quota')
-			self.abort(
-				code=403,
-				detail='over internal quota',
-			)
+		if self.quota:
+			if count >= self.quota.count:
+				logging.warning('over internal quota count %d', self.quota.count)
+				self.abort(
+					code=403,
+					detail='over internal quota',
+				)
 
 	def _abort_incorrect_client(self):
 		self.abort(
 			code=403,
 			detail='Please request via XmlHttpRequest Lv.2 API',
 		)
-
-	def get(self, url):
-		self._prepare_quota()
-		self._update_quota_limits()
-
-		if self.request.query_string:
-			url += '?' + self.request.query_string
-
-		logging.debug('url %s', url)
-		logging.debug('referrer %s', self.request.headers.get('referer', ''))
-
-		origin = self.request.headers.get('origin', '').lower();
-		logging.debug('origin %s', origin)
+	
+	def _check_request(self, origin):
 		if not origin:
-			logging.warning('missing origin')
-			self._abort_incorrect_client();
+			logging.info('missing origin')
 			return
 
 		user_agent = self.request.headers.get('user-agent', '')
@@ -93,43 +81,67 @@ class HttpProxyHandler(webapp2.RequestHandler):
 			self._abort_incorrect_client();
 			return
 
-		self._check_quota(origin)
-
-		res, errorReason = None, None
+	def _urlfetch(self, url):
+		key = 'urlfetch:%s' % url
+		uresp = memcache.get(key)
+		if uresp is not None:
+			logging.debug('urlfetch cache hit')
+			return uresp
+	
+		res = None
 		try:
+			logging.debug('urlfetch cache miss')
 			res = urlfetch.fetch(url)
 
 		except (urlfetch.InvalidURLError,
 			urlfetch.DownloadError,
 			urlfetch.ResponseTooLargeError,
-			apiproxy_errors.DeadlineExceededError), error:
+			apiproxy_errors.DeadlineExceededError), err:
 
-			errorReason = repr(error)
-
-		if res:
-			for key, val in res.headers.iteritems():
-				self.response.headers[key] = val
-
-			self.response.headers['Access-Control-Allow-Origin'] = '*'
-			self.response.headers.pop('Set-Cookie', None)
-
-			if res.status_code >= 500:
-				logging.info('override response code %d', res.status_code)
-
-				self.response.headers['X-Original-Status-Code'] = str(res.status_code)
-				self.response.set_status(403)
-			else:
-				self.response.set_status(res.status_code)
-
-			self.response.write(res.content)
-
-		else:
-			logging.warning('urlfetch error: %s', errorReason)
-
+			logging.warning('urlfetch error: %r', err)
 			self.abort(
 				code=403,
-				detail='urlfetch error: %s' % errorReason,
+				detail='urlfetch error: %r' % err,
 			)
+
+		uresp = UrlResp(res)
+		try:
+			memcache.set(key, uresp, self.urlfetch_cache_seconds)
+
+		except ValueError, err:
+			logging.warning('memcache error: %r', err)
+
+		return uresp
+
+	def get(self, url):
+		if self.request.query_string:
+			url += '?' + self.request.query_string
+
+		logging.debug('url %s', url)
+		logging.debug('referrer %s', self.request.headers.get('referer', ''))
+		origin = self.request.headers.get('origin', '').lower();
+		logging.debug('origin %s', origin)
+
+		self._check_request(origin)
+		self._refresh_quota()
+		self._check_quota(origin)
+
+		uresp = self._urlfetch(url)
+		for key, val in uresp.headers.iteritems():
+			self.response.headers[key] = val
+
+		self.response.headers['access-control-allow-origin'] = '*'
+		self.response.headers.pop('set-cookie', None)
+
+		if uresp.status_code >= 500:
+			logging.info('override response code %d', uresp.status_code)
+
+			self.response.headers['x-original-status-code'] = str(uresp.status_code)
+			self.response.set_status(403)
+		else:
+			self.response.set_status(uresp.status_code)
+
+		self.response.write(uresp.content)
 
 class OkHandler(webapp2.RequestHandler):
 	def get(self):
@@ -142,7 +154,9 @@ class OkHandler(webapp2.RequestHandler):
 	head = get
 
 Quota.get_or_insert(Quota.default_id,
-	count=1000, reset_interval=60 * 60 * 2)
+	count=1000,
+	reset_interval=3600
+)
 
 application = webapp2.WSGIApplication([
 	(r'/(https?://.*)', HttpProxyHandler),
